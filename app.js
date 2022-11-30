ce2e198e58aeebfb058f4dc4b7725095b3f4580d
prathmesh <71115267+pawarprathmesh333@users.noreply.github.com>
2022-11-30 11:57:14 +0530

    chore - app.js done

---
+++ app.js
@@ -0,0 +1,29 @@
+const birthDate = document.querySelector("#birth-date");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkButton = document.querySelector("#check");
+const responce = document.querySelector("#responce");
+
+function calculateSum(dob){
+    dob = dob.replaceAll("-","");
+    let sum =0;
+    for(let i=0;i<dob.length;i++){
+        sum += Number(dob[i]);
+    }
+    console.log(sum);
+    return sum;
+}
+
+checkButton.addEventListener("click", function (){
+    if(birthDate.value&&luckyNumber.value)
+    {    
+        const dob = birthDate.value;
+        const sum = calculateSum(dob);
+        if(sum%luckyNumber.value==0){
+            responce.innerText = "Your Birthdate is lucky🤞.";
+        }else{
+            responce.innerText = "Your Birthdate is Not that lucky😬.";
+        }
+    }else{
+        responce.innerText = "Enter both inputs😡.";
+    }
+})
